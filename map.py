c23ed6120c464cb293a28d6b490f14eb90256ac7
Danish Khan <danish.khan@sjsu.edu>
2020-10-14 17:35:25 -0700

    minor edits

--- map.py
+++ map.py
@@ -18,8 +18,8 @@ API_KEY='AIzaSyDbVjEnGw9CHvVgECZoDr8LuL0DGXZYzbo'  #Google Maps Directions API K
 m = folium.Map(location=[42.3601, -71.0589], zoom_start=12)
 
 # Global tooltip
-tooltip = 'Click For More Info'
-
+tooltip1 = 'Starting Point'
+tooltip2 = 'Destination'
 # Create custom marker icon
 logoIcon = folium.features.CustomIcon('logo.png', icon_size=(50, 50))
 
@@ -32,22 +32,22 @@ overlay = os.path.join('data', 'overlay.json')
 # Create markers
 folium.Marker([42.363600, -71.099500],
               popup='<strong>Location One</strong>',
-              tooltip=tooltip).add_to(m),
+              tooltip=tooltip1).add_to(m),
 folium.Marker([42.333600, -71.109500],
               popup='<strong>Location Two</strong>',
-              tooltip=tooltip,
+              tooltip=tooltip2,
               icon=folium.Icon(icon='cloud')).add_to(m),
 folium.Marker([42.377120, -71.062400],
               popup='<strong>Location Three</strong>',
-              tooltip=tooltip,
+              tooltip=tooltip1,
               icon=folium.Icon(color='purple')).add_to(m),
 folium.Marker([42.374150, -71.122410],
               popup='<strong>Location Four</strong>',
-              tooltip=tooltip,
+              tooltip=tooltip2,
               icon=folium.Icon(color='green', icon='leaf')).add_to(m),
 folium.Marker([42.375140, -71.032450],
               popup='<strong>Location Five</strong>',
-              tooltip=tooltip,
+              tooltip=tooltip1,
               icon=logoIcon).add_to(m),
 folium.Marker([42.315140, -71.072450],
               popup=folium.Popup(max_width=450).add_child(folium.Vega(json.load(open(vis)), width=450, height=250))).add_to(m)
@@ -56,10 +56,10 @@ folium.Marker([42.315140, -71.072450],
 folium.CircleMarker(
     location=[42.466470, -70.942110],
     radius=50,
-    popup='My Birthplace',
-    color='#428bca',
+    popup='Sample Circle Marker',
+    color='#FFFFFF',
     fill=True,
-    fill_color='#428bca'
+    fill_color='#FFFFFF'
 ).add_to(m)
 
 # Geojson overlay
