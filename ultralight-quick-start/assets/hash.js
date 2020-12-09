5dbb03f07b060700e5d29b3809396d966f9a85bd
Christiana Huiskens <christiana.huiskens@sjsu.edu>
2020-12-08 23:07:18 -0800

    bucket chaining

--- ultralight-quick-start/assets/hash.js
+++ ultralight-quick-start/assets/hash.js
@@ -10,6 +10,8 @@ http.createServer(function (req, res) {
 class Hash {
     constructor() {
         this.map = {}
+        this.length = 0;
+        this.size = 0;
         
     }
 
@@ -20,17 +22,19 @@ class Hash {
             total += H * total + string.charCodeAt(i);// charCodeAt script return a character’s ASCII value after multiplying the ASCII value by a multiplier H
         }
         total %= this.table.length;
-        if (total < 1) this.table.length - 1
+        if (total < 1) this.table.length - 1;
         return parseInt(total);
     }
 
     showDistro() {
         for (const key in this.table) {
-            if (this.table[key] !== undefined) { console.log(key, ' : ', this.table[key]);}
+            if (this.table[key] !== undefined) { 
+                console.log(key, ' : ', this.table[key]);
+            }
         }
     }
 
-    put(data) {
+    store(data) { // stores only after there is no collisons
         const pos = this.hash(data);
         this.table[pos] = data;
     }
@@ -40,20 +44,69 @@ class Hash {
 
     }
 
-    add(key, value) {
-        let size = this.hash(key)
+    add(key, value) { // maybe not be necessary 
+        let size = this.hash(key);
         if (!this.map[size]) {
-            this.map[k] = []
+            this.map[k] = [];
         }
-        this.map[size].push(value)
+        this.map[size].push(value);
     }
-    get(key) {
-        let size = this.hash(key)
-        return this.map[k]
+    search(key) {
+        let size = this.hash(key);
+        return this.map[k];
     }
 }
 
 let p = new Hash()
+
+//bucket chaining for resizing
+class linear extends Hash {
+    constructor(){
+        super(); // keyword is used to access and call functions on an object's parent (avoid duplications)
+        this.values = new array();
+    }
+
+    build_bucketchains(){
+        for(var i = 0; i < this.table.length; i++){
+            this.table[i] = new array();
+        }
+    }
+    showDistro() {
+        for (const key in this.table) {
+            if (this.table[key] !== undefined) { 
+                console.log(key, ' : ', this.table[key]);
+            }
+        }
+    }
+
+    store(key, data){
+        const position = this.hash(key);
+             if(this.table[position] === undefined){
+                this.table[position] = key;
+                this.values[position] = data;
+            }
+        else {
+            while (this.table[position] !== undefined){
+                position++;
+             }
+            this.table[position][index] = data;
+        }
+    }
+
+    search(key){
+        const position = this.hash(key);
+        let index = 0;
+        if (this.table[position][index] != key && this.table[position][index] != NULL){
+            return this.table[position][index];
+            index++;
+        } 
+        else { 
+            return null;
+            index++;
+        }
+    }
+}
+
 
 
 
